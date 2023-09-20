d23a06e255d624ee0338aa716a963f9dff1f59ef
SergioBogaro <sergio.bogaro@smartinnovation.com.br>
2023-09-20 20:43:48 -0300

    note

--- Scenes/Managers/EnemyManager.gd
+++ Scenes/Managers/EnemyManager.gd
@@ -1,17 +1,25 @@
 extends Node
 
+@onready var EnemyTimer: Timer = $Timer
+
 @export var enemyList: PackedScene
+@export var TimeManager : Node
 
 const SPAWN_RADIUS = 330
 
 var player : CharacterBody2D
 var entitiesLayer: Node2D
+var baseWaitTime: int = 0
 
 func _ready():
 	GetData()
-	$Timer.timeout.connect(SpawnTimerTimeout)
+	baseWaitTime = EnemyTimer.wait_time
+	EnemyTimer.timeout.connect(SpawnTimerTimeout)
+	TimeManager.game_difficult_increased.connect(onGameDifficultIncreased)
 
 func SpawnTimerTimeout():
+	EnemyTimer.start()
+	
 	if !player or !entitiesLayer:
 		GetData()
 		return
@@ -29,3 +37,8 @@ func GetData():
 		return
 	
 	entitiesLayer = get_tree().get_first_node_in_group("EntitiesLayer")
+
+func onGameDifficultIncreased(gameDifficult : int):
+	var timeOff = 0.1 / 12
+	# timeOff = max(0.7, timeOff)
+	EnemyTimer.wait_time = baseWaitTime - timeOff
--- Scenes/Managers/TimeManager.gd
+++ Scenes/Managers/TimeManager.gd
@@ -1,6 +1,18 @@
 extends Node
 
+signal game_difficult_increased(gameDifficult : int)
+
+const DIFICULT_INTERVAL : int = 5
+
 @onready var GameTimer: Timer = $Timer
+
+var gameDifficult: int = 0
+
+func _process(delta):
+	var nextTimeTarged = GameTimer.wait_time - ((gameDifficult + 1) * DIFICULT_INTERVAL)
+	if GameTimer.time_left <= nextTimeTarged:
+		gameDifficult += 1
+		game_difficult_increased.emit(gameDifficult)
 
 func _ready():
 	GameTimer.timeout.connect(onGameTimerTimeout)
